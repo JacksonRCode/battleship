a6798d1bf93dc86d70eb7100192f0c6d98a729df
JacksonRCode <jackdareid@gmail.com>
2025-03-21 14:35:47 -0400

    Configuration files were messed up. Fixed them

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import "./styles/baseStyles.css";
-import "./styles/styles.css";
+// import "./styles/baseStyles.css";
+// import "./styles/styles.css";
+// import Player from "./Player";
+// import Ship from "./Ship";
+// import Board from "./Board.js";
+
+// (initGame = () => {
+//   console.log("yo");
+// })();
