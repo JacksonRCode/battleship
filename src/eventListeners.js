c28d7f7f864dbf090069b6b4aefd75991b18b687
JacksonRCode <jackdareid@gmail.com>
2025-04-08 11:07:25 -0400

    Implement dynamic player creation rather than statically creating them in the index.js file

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -7,12 +7,13 @@ import {
   startGame,
 } from "./control.js";
 import { Board } from "./Board.js";
+import { Player } from "./Player.js";
 
 const P1BOARD = document.querySelector(".player1");
 const P2BOARD = document.querySelector(".player2");
 const SHIPUI = document.querySelector(".place-ships");
 
-const initListeners = (player1, player2) => {
+const initListeners = () => {
   const playerTwoInput = document.querySelector(".p2-input-container");
   document.querySelector("#check-two-player").addEventListener("click", () => {
     // Checks to see if one or two players are playing
@@ -24,21 +25,44 @@ const initListeners = (player1, player2) => {
     }
   });
 
+  // This exits out of player creation screen
   document
     .querySelector(".go-to-ship-selection")
     .addEventListener("click", () => {
       // Make previous page invisible:
       document.querySelector(".create-players").classList.add("invisible");
 
+      // Check to see if there are 1 or 2 players
+      const twoPlayer = document.getElementById("check-two-player").checked;
+
+      // Get player 1's username
+      let p1Name = document.getElementById("p1-input").value;
+      p1Name = p1Name === "" ? "Player 1" : p1Name;
+
+      // Get player 2's username if there are 2 players
+      let p2Name = "Boat Robot";
+      if (twoPlayer) {
+        p2Name = document.getElementById("p2-input").value;
+      }
+
+      // Create players
+      const p1 = Player(p1Name, true);
+      const p2 = Player(p2Name, twoPlayer);
+
       // Create new board and begin ship selection with it
       const p1Board = Board(10);
       initShipSelection(p1Board);
-      player1.assignBoard(p1Board);
+      p1.assignBoard(p1Board);
 
       // Make ship selection visible
       SHIPUI.classList.remove("invisible");
-    });
 
+      // Initialize other event listeners now that players have been created
+      playerDependentListeners(p1, p2);
+    });
+};
+
+const playerDependentListeners = (player1, player2) => {
   const selectionNextButton = document.querySelector(".p2-board-selection");
   selectionNextButton.addEventListener("click", () => {
     // Check whether or not there is a second player:
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Ship } from "./Ship.js";
 import { Board } from "./Board.js";
 import { initListeners } from "./eventListeners.js";
 
-const BOARD_DIMENSIONS = 10;
+// const BOARD_DIMENSIONS = 10;
 
 const player1 = Player("Jackson", true);
 const playerBot = Player("Optimus", false);
@@ -13,48 +13,48 @@ const playerBot = Player("Optimus", false);
 // ships = [[ship object, ship coordinates],[],[]]
 
 // Create Player ships
-const pShip1 = Ship(3, "Battleship");
-const pShip1Coords = [
-  [0, 0],
-  [0, 1],
-  [0, 2],
-];
-const pShip2 = Ship(2, "Little Ship");
-const pShip2Coords = [
-  [1, 0],
-  [1, 1],
-];
-const pShip3 = Ship(4, "Big Boat!");
-const pShip3Coords = [
-  [2, 0],
-  [2, 1],
-  [2, 2],
-  [2, 3],
-];
-const pShips = [
-  [pShip1, pShip1Coords],
-  [pShip2, pShip2Coords],
-  [pShip3, pShip3Coords],
-];
+// const pShip1 = Ship(3, "Battleship");
+// const pShip1Coords = [
+//   [0, 0],
+//   [0, 1],
+//   [0, 2],
+// ];
+// const pShip2 = Ship(2, "Little Ship");
+// const pShip2Coords = [
+//   [1, 0],
+//   [1, 1],
+// ];
+// const pShip3 = Ship(4, "Big Boat!");
+// const pShip3Coords = [
+//   [2, 0],
+//   [2, 1],
+//   [2, 2],
+//   [2, 3],
+// ];
+// const pShips = [
+//   [pShip1, pShip1Coords],
+//   [pShip2, pShip2Coords],
+//   [pShip3, pShip3Coords],
+// ];
 
 // Create Player Board
-const pBoard = Board(BOARD_DIMENSIONS, pShips);
+// const pBoard = Board(BOARD_DIMENSIONS, pShips);
 
 // Add Board to Player
 // player1.assignBoard(pBoard);
 
 // Create Bot Ships
-const bShip1 = Ship(3, "Battleship");
-const bShip2 = Ship(2, "Little Ship");
-const bShip3 = Ship(4, "Big Boat!");
-const bShips = [
-  [bShip1, pShip1Coords],
-  [bShip2, pShip2Coords],
-  [bShip3, pShip3Coords],
-];
+// const bShip1 = Ship(3, "Battleship");
+// const bShip2 = Ship(2, "Little Ship");
+// const bShip3 = Ship(4, "Big Boat!");
+// const bShips = [
+//   [bShip1, pShip1Coords],
+//   [bShip2, pShip2Coords],
+//   [bShip3, pShip3Coords],
+// ];
 
 // Create Bot Board
-const bBoard = Board(BOARD_DIMENSIONS, bShips);
+// const bBoard = Board(BOARD_DIMENSIONS, bShips);
 
 // Add Board to Bot
 // playerBot.assignBoard(bBoard);
